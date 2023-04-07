5496ed78633a902fb81f416096433aab8253e1ea
Pedro Murta <41590357+PedroMurta@users.noreply.github.com>
2023-04-07 00:25:39 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, render_template
+import streamlit as st
+import requests
 
 app = Flask(__name__)
 
+# Define a rota inicial
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    # Utiliza o pacote requests para fazer a requisição do Streamlit
+    response = requests.get("http://localhost:8501/")
+    # Retorna o conteúdo do Streamlit como uma string
+    return response.text
+
+# Define a rota para renderizar o Streamlit
+@app.route("/streamlit")
+def streamlit():
+    # Utiliza o pacote streamlit para renderizar a página desejada
+    st.set_page_config(page_title="Desafio Analista", page_icon=":pencil:", layout="wide")
+    from streamlit_app import main
+    main()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8501)
